9c1cdda0c686024ed037c35a6030f14291a0d433
lauricdd <lauri_cdd@hotmail.com>
2016-04-04 15:52:59 -0500

    vista de estudiantes

--- public_html/js/init.js
+++ public_html/js/init.js
@@ -2,6 +2,8 @@
     $(function(){
 
         $('.button-collapse').sideNav();
+        
+        $('select').material_select();
 
     }); // end of document ready
 })(jQuery); // end of jQuery name space
